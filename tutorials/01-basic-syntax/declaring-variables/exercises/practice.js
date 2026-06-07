a2254f7c23787728450f8174b59d4547fae967a2
Rekha <rekhavs003@gmail.com>
2026-06-07 07:26:06 -0500

    practice done

--- tutorials/01-basic-syntax/declaring-variables/exercises/practice.js
+++ tutorials/01-basic-syntax/declaring-variables/exercises/practice.js
@@ -15,9 +15,17 @@
 // of one of your favorite movies, assign a value, then print the
 // variable to the console to see its stored value.
 
+let myFavouriteMovie = "Toy story";
+console.log (myFavouriteMovie);
+
 // 2. TODO: Create a variable called futureVacationSpot that stores a location
 // you plan to visit one day, assign a value, then print the variable to the
 // console to see its stored value.
+let futureVacationSpot = "Italy";
+let numberOfDays = 1;
+
+console.log (futureVacationSpot);
+console.log (numberOfDays);
 
 // 3. TODO: Create a variable called perfectTemp that stores your ideal temperature
 // outside on a 'perfect day', assign a value, then print the variable
@@ -25,11 +33,17 @@
 
 // 4A. TODO: Declare a variable called numberOfStatesVisited, but do not initialize it yet.
 
+let numberOfStatesVisited;
+console.log (numberOfStatesVisited);
+
 // 4B. TODO: Think about how many states in the U.S. you've been to. For some of us
 // this could be a pretty big number that requires a lot of thought! Here's a tool you can use:
 // https://www.fla-shop.com/visited-states/?vc=1ca032&uc=90cfea&hc=40bfa6&bc=ffffff&ss=on&sl=on
 // Once you have the correct total, write another line of code below to assign it to the variable you
 // created above. Then print the variable to the console to see its stored value.
+
+numberOfStatesVisited = 10;
+console.log (numberOfStatesVisited);
 
 // Great job! You will get LOTS more practice with this in future exercises.
 
